bb3018bb2db4e80fb974b3b8a40573e1bd86f57f
Manfred Bergmann <mb@software-by-mabe.com>
2019-11-02 09:48:59 +0100

    initial import

---
+++ lib/elixir_gol.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGol do
+  @moduledoc """
+  Documentation for ElixirGol.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGol.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_gol_test.exs
@@ -0,0 +1,22 @@
+defmodule ElixirGolTest do
+  use ExUnit.Case
+  import Mock
+
+  doctest ElixirGol
+
+  test "greets the world" do
+    assert ElixirGol.hello() == :world
+  end
+
+  test "get" do
+    with_mock HTTPotion,
+      [get: fn("http://example.com", _headers) ->
+        HTTPotion.Response.new(status_code: 200,
+          body: "hello") end] do
+      # Code which calls HTTPotion.get
+      # Check that the call was made as we expected
+      assert called HTTPotion.get("http://example.com", :_)
+    end
+  end
+  
+end
